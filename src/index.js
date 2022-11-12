531388bf55b5ac4731276725290d2aeb05153654
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-11-11 21:37:37 -0300

    Renamed div with id 'app' to 'root'

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import Footer from './Footer';
 import Header from "./Header";
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Header />
